758b83cbc67a9f27c0c567342dadade70e64a463
Mark Cruz <markchriscruz@yahoo.com>
2018-03-06 21:56:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,15 @@
-# Write your code here.
 def line(array)
+  string = ""
   if array.length == 0
     puts "The line is currently empty."
   else
+    string = "The line is currently: "
     array.each_with_index do |element, index|
+      string = string + (index + 1).to_s + ". #{element} "
+    end
     # do nothing
   end
+  return string
 end
 
 def take_a_number(array, string)
