168fc6a3597a689d5d3d293d2baba12ad3588321
Mark Cruz <markchriscruz@yahoo.com>
2018-03-06 16:08:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    # do nothing
+  end
+end
+
+def take_a_number(array, string)
+  
+end
+
